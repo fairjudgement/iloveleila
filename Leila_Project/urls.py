07546b258b203083211efa054fd58c2c5aabb617
fairjudgement <canozgunyilmaz@gmail.com>
2017-02-16 16:35:01 +0300

    hadi

--- Leila_Project/urls.py
+++ Leila_Project/urls.py
@@ -20,8 +20,9 @@ from Leila.forms import LoginForm
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('Leila.urls')),
-    url(r'^login/$', views.login, {'template_name': 'Leila/login.html', 'authentication_form': LoginForm}),
-    url(r'^/login/$', 'django.contrib.auth.views.login', name='login'),
+    #url(r'^login/$', views.login, {'template_name': 'Leila/login.html', 'authentication_form': LoginForm}),
+    url(r'^login/$', views.login, {'template_name': 'Leila/login.html', 'authentication_form': LoginForm},
+        name="login"),
     url(r'^logout/$', views.logout, {'next_page': '/login'}),
 
 ]
